e115307329da4f67a8ca18deb882ef88d1f58703
Florian Pechwitz <florian.pechwitz@itizzimo.com>
2019-05-06 14:11:41 +0200

    - Code clean up

--- hooks/ios/addBuildScript.js
+++ hooks/ios/addBuildScript.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (ctx) {
-    var deferral = undefined;
-    var cordovaVersion = ctx.opts.cordova.version.split(".");
 
     var filterFilesFromDir = function (startPath, filter, rec, multiple) {
         if (!fs.existsSync(startPath)) {
@@ -35,18 +33,12 @@ module.exports = function (ctx) {
         }
     };
 
-    const xcodeProjPath = filterFilesFromDir('platforms/ios', '.xcodeproj', false);
-    const projectPath = xcodeProjPath + '/project.pbxproj';
-    const myProj = xcode.project(projectPath);
-    console.log("Fetching build script removeArchitectures.sh ...");
-
-    var addingScript = function(successCallback, errorCallback) {
-        fs.readFile(process.cwd() + "/plugins/cordova-plugin-build-filter/hooks/ios/removeArchitectures.sh", "utf8", function(err, contents){
+    var addingScript = function (successCallback, errorCallback) {
+        fs.readFile(process.cwd() + "/plugins/cordova-plugin-build-filter/hooks/ios/removeArchitectures.sh", "utf8", function (err, contents) {
             if (err) {
                 console.error(err);
 
                 return errorCallback(err);
-                //return deferral.reject(err);
             }
 
             console.log("Adding build script to Xcode project file ...");
@@ -54,24 +46,30 @@ module.exports = function (ctx) {
             var options = {shellPath: '/bin/sh', shellScript: contents};
 
             myProj.parse(function (err) {
-                if(err){
+                if (err) {
                     console.error(err);
                     return errorCallback(err);
-                    //return deferral.reject(err);
                 }
 
                 myProj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Remove not required architecures (Script)', myProj.getFirstTarget().uuid, options);
                 fs.writeFileSync(projectPath, myProj.writeSync());
                 console.log("Added build script to Xcode project file");
                 return successCallback();
-                //return deferral.resolve();
             });
         });
     };
 
-    if(Number(cordovaVersion[0]) <= 8) {
-        deferral = ctx.requireCordovaModule('q').defer();
-        addingScript(function() {
+    const xcodeProjPath = filterFilesFromDir('platforms/ios', '.xcodeproj', false);
+    const projectPath = xcodeProjPath + '/project.pbxproj';
+    const myProj = xcode.project(projectPath);
+    console.log("Fetching build script removeArchitectures.sh ...");
+
+    const cordovaVersion = ctx.opts.cordova.version.split(".");
+
+    //Q is not supported on Cordova CLI 9 anymore so use Promise instead
+    if (Number(cordovaVersion[0]) <= 8) {
+        var deferral = ctx.requireCordovaModule('q').defer();
+        addingScript(function () {
             return deferral.resolve();
         }, function (error) {
             return deferral.reject(error)
@@ -79,11 +77,11 @@ module.exports = function (ctx) {
         return deferral.promise;
     } else {
         return new Promise(function (resolve, reject) {
-           addingScript(function () {
-               resolve();
-           }, function (error) {
-              reject(error);
-           });
+            addingScript(function () {
+                resolve();
+            }, function (error) {
+                reject(error);
+            });
         });
     }
 };
