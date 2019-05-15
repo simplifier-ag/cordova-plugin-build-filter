95cfb9c3b5d9ccf9138a45f20f6da0ef2d540756
Paul Stresow <paulstresow@Pauls-Mac-mini.local>
2019-05-15 11:01:24 +0200

    changed to promise and sync, changed hook to after_plugin_install

--- hooks/ios/addBuildScript.js
+++ hooks/ios/addBuildScript.js
@@ -2,87 +2,60 @@ var xcode = require('xcode');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function (ctx) {
+const filterFilesFromDir = function (startPath, filter, rec, multiple) {
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
+        return;
+    }
 
-    var filterFilesFromDir = function (startPath, filter, rec, multiple) {
-        if (!fs.existsSync(startPath)) {
-            console.log("no dir ", startPath);
-            return;
+    var files = fs.readdirSync(startPath);
+    var resultFiles = [];
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory() && rec) {
+            filterFilesFromDir(filename, filter); //recurse
         }
 
-        var files = fs.readdirSync(startPath);
-        var resultFiles = [];
-        for (var i = 0; i < files.length; i++) {
-            var filename = path.join(startPath, files[i]);
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory() && rec) {
-                filterFilesFromDir(filename, filter); //recurse
-            }
-
-            if (filename.indexOf(filter) >= 0) {
-                if (multiple) {
-                    resultFiles.push(filename);
-                } else {
-                    return filename;
-                }
+        if (filename.indexOf(filter) >= 0) {
+            if (multiple) {
+                resultFiles.push(filename);
+            } else {
+                return filename;
             }
         }
+    }
 
-        if (multiple) {
-            return resultFiles;
-        }
-    };
+    if (multiple) {
+        return resultFiles;
+    }
+};
 
-    var addingScript = function (successCallback, errorCallback) {
+module.exports = ctx => {
+    return new Promise((resolve, reject) => {
+        const xcodeProjPath = filterFilesFromDir('platforms/ios', '.xcodeproj', false);
+        const projectPath = xcodeProjPath + '/project.pbxproj';
+        const myProj = xcode.project(projectPath);
+        console.log("Fetching build script removeArchitectures.sh ...");
+
         fs.readFile(process.cwd() + "/plugins/cordova-plugin-build-filter/hooks/ios/removeArchitectures.sh", "utf8", function (err, contents) {
             if (err) {
-                console.error(err);
-
-                return errorCallback(err);
+                return reject(err);
             }
 
             console.log("Adding build script to Xcode project file ...");
 
-            var options = {shellPath: '/bin/sh', shellScript: contents};
+            let options = {shellPath: '/bin/sh', shellScript: contents};
 
-            myProj.parse(function (err) {
-                if (err) {
-                    console.error(err);
-                    return errorCallback(err);
-                }
-
-                myProj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Remove not required architecures (Script)', myProj.getFirstTarget().uuid, options);
-                fs.writeFileSync(projectPath, myProj.writeSync());
-                console.log("Added build script to Xcode project file");
-                return successCallback();
-            });
+            myProj.parseSync();
+            myProj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Remove not required architecures (Script)', myProj.getFirstTarget().uuid, options);
+            fs.writeFileSync(projectPath, myProj.writeSync());
+            return resolve("Added build script to Xcode project file");
         });
-    };
 
-    const xcodeProjPath = filterFilesFromDir('platforms/ios', '.xcodeproj', false);
-    const projectPath = xcodeProjPath + '/project.pbxproj';
-    const myProj = xcode.project(projectPath);
-    console.log("Fetching build script removeArchitectures.sh ...");
-
-    const cordovaVersion = ctx.opts.cordova.version.split(".");
-
-    //Q is not supported on Cordova CLI 9 anymore so use Promise instead
-    if (Number(cordovaVersion[0]) <= 8) {
-        var deferral = ctx.requireCordovaModule('q').defer();
-        addingScript(function () {
-            return deferral.resolve();
-        }, function (error) {
-            return deferral.reject(error)
-        });
-        return deferral.promise;
-    } else {
-        return new Promise(function (resolve, reject) {
-            addingScript(function () {
-                resolve();
-            }, function (error) {
-                reject(error);
-            });
-        });
-    }
+    }).then(message => {
+        console.log(`${message}`);
+    }).catch(message => {
+        console.warn(`${message}`);
+    });
 };
-
